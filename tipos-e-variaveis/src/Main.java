b0c41bf2b937091186d555dbce95929b31f89168
Nícolas Santos <nicollas.ricardo@hotmail.com>
2024-05-15 11:21:32 -0300

    Operadores

--- tipos-e-variaveis/src/Main.java
+++ tipos-e-variaveis/src/Main.java
@@ -1,9 +1,20 @@
 
 public class Main {
+
     public static void main(String[] args) {
 
-        System.out.printf("Estudando no horario de trabalho.");
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+
+        int num = 5;
+
+        num = 22;
+
+        System.out.println(num);
+
+        final double VALOR_DE_PI = 3.14;
 
         }
     }
-}
---
+++ tipos-e-variaveis/src/Operadores.java
@@ -0,0 +1,18 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+
+        boolean condicao1 = false;
+
+        boolean condicao2 = false;
+
+        if (condicao1 && condicao2) {
+            System.out.println("As condições são verdadeiras.");
+        } else if (condicao1 || condicao2) {
+            System.out.println("Uma das condições são verdadeiras!");
+        }else {
+            System.out.println("Nenhuma condição é verdadeira");
+        }
+
+    }
+}
