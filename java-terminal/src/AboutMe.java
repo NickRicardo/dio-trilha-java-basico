bcc53611faa7ff981373ece071c248bd1f4740da
Nícolas Santos <nicollas.ricardo@hotmail.com>
2024-05-16 08:45:55 -0300

    Desafio Sintaxe

--- java-terminal/src/AboutMe.java
+++ java-terminal/src/AboutMe.java
@@ -2,28 +2,9 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class AboutMe {
-    public static void main(String[] args) {
-        //criando o objeto scanner
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-
-        System.out.println("Digite seu nome");
-        String nome = scanner.next();
-
-        System.out.println("Digite seu sobrenome");
-        String sobrenome = scanner.next();
-
-        System.out.println("Digite sua idade");
-        int idade = scanner.nextInt();
-
-        System.out.println("Digite sua altura");
-        double altura = scanner.nextDouble();
-
-
-        //imprimindo os dados obtidos pelo usuario
-        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
-        System.out.println("Tenho " + idade + " anos ");
-        System.out.println("Minha altura é " + altura + "cm ");
-
-
+    public static void main(String[] args) throws Exception {
+        int numero1 = 1;
+        String numero2 = "2";
+        System.out.println(numero1+numero2);
     }
 }
