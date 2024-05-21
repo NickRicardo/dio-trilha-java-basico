27744b5f05f276ce87c881feed67251e99738e35
Nícolas Santos <nicollas.ricardo@hotmail.com>
2024-05-21 17:58:04 -0300

    controle-de-fluxo1

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,11 +1,34 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
 
     public static void main(String[] args) {
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
+        selecaoCandidato();
+    }
+
+    static void selecaoCandidato() {
+        String[] candidatos = {"FELIPE", "Nicolas", "Duda", "Bela", "Perci", "Daniboy", "Gobbi"};
+
+        int candidatoSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+        while (candidatoSelecionados < 5) {
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("o Candidato " + candidato + " solicitou este valor de salario " + salarioPretendido);
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O Candidato " + candidato + " foi selecionado para a vaga ");
+                candidatoSelecionados++;
+            }
+            candidatoAtual++;
+        }
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
 
     static void analisarCandidato(double salarioPretendido) {
