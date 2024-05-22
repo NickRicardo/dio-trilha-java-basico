a7b173c8a022c6367a345a37a02d56c837a47f1d
Nícolas Santos <nicollas.ricardo@hotmail.com>
2024-05-22 10:08:46 -0300

    controle-de-fluxo2

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,11 +1,56 @@
 package candidatura;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
 
     public static void main(String[] args) {
-        selecaoCandidato();
+        String[] candidatos = {"Felipe", "Nícolas", "Keilla", "Laura", "Naomi"};
+        for (String candidato : candidatos) {
+            entrandoEmContato(candidato);
+        }
+    }
+
+    static void entrandoEmContato(String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;
+
+            if (continuarTentando)
+                tentativasRealizadas++;
+            else
+                System.out.println("Contato Realizado com Sucesso");
+
+        } while (continuarTentando && tentativasRealizadas < 3);
+
+        if (atendeu)
+            System.out.println("Conseguimos contato com " + candidato + " na " + tentativasRealizadas + " tentativa");
+        else
+            System.out.println("Não conseguimos contato com " + candidato + ", número maximo de tentativas " + tentativasRealizadas + " realizada");
+    }
+
+    //metodo auxiliar
+    static boolean atender() {
+        return new Random().nextInt(3) == 1;
+    }
+
+    static void imprimirSelecionados() {
+        String[] candidatos = {"FELIPE", "Nicolas", "Duda", "Bela", "Perci"};
+
+        System.out.println("Imprimindo a lista de candidatos informando o índice do elemento");
+        for (int indice = 0; indice < candidatos.length; indice++) {
+            System.out.println("O Candidato de nº " + (indice + 1) + " é o" + candidatos[indice]);
+        }
+
+        System.out.println("Forma abreviada de interação For Each");
+
+        for (String candidato : candidatos) {
+            System.out.println("O Candidato selecionado foi " + candidato);
+        }
     }
 
     static void selecaoCandidato() {
