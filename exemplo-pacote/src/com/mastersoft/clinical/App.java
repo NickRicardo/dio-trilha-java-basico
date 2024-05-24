5c350530a7a2e603cbdacfea24cfbf52c2cb7592
Nícolas Santos <nicollas.ricardo@hotmail.com>
2024-05-24 15:48:18 -0300

    POO

---
+++ exemplo-pacote/src/com/mastersoft/clinical/App.java
@@ -0,0 +1,11 @@
+package com.mastersoft.clinical;
+
+import com.mastersoft.clinical.model.Exame;
+
+public class App {
+    public static void main(String[] args) {
+
+        Exame exame = new Exame();
+
+    }
+}
