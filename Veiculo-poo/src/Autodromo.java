3274045935983a77f6b0ba5f160d82de515f1116
Nícolas Santos <nicollas.ricardo@hotmail.com>
2024-05-28 16:31:07 -0300

    Encapsulamento, Herença, Abstração e Polimorfismo

---
+++ Veiculo-poo/src/Autodromo.java
@@ -0,0 +1,19 @@
+public class Autodromo {
+
+    public static void main(String[] args) {
+
+        Carro jeep = new Carro();
+        jeep.setChassi("879879");
+//        jeep.ligar();
+
+
+        Moto z400 = new Moto();
+        z400.setChassi("654654");
+//        z400.ligar();
+
+        Veiculo coringa = jeep;
+
+        coringa.ligar();
+
+    }
+}
